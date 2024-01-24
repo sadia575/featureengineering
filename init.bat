8d9fd32add422306c9e80ec1181a4dec4b341e7c
sadia575 <sadie.halim@yahoo.com>
2024-01-24 17:42:16 +0300

    modified:   init.bat

--- init.bat
+++ init.bat
@@ -1,3 +1,3 @@
-call conda create featureEngineeringMethods python3.11
+call conda create -n featureEngineeringMethods python=3.11
 call conda activate featureEngineeringMethods
-pip install requirements.txt
+pip install -r requirements.txt
